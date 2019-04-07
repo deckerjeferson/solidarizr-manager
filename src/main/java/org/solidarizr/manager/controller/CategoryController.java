77a5ffa59caf643828aa283991d77c35a2a27caf
Jeferson Decker <deckerjeferson@gmail.com>
2019-04-07 00:21:03 -0300

    Finishing the Category Controller

--- src/main/java/org/solidarizr/manager/controller/CategoryController.java
+++ src/main/java/org/solidarizr/manager/controller/CategoryController.java
@@ -4,7 +4,11 @@ import org.solidarizr.manager.model.Category;
 import org.solidarizr.manager.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 @RequestMapping
@@ -17,7 +21,19 @@ public class CategoryController {
         this.service = service;
     }
 
-    public Category save(Category categoryToBeInserted) {
-        return null;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Category save(@RequestBody Category category) {
+        return service.save(category);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    public void delete(@RequestBody Category category) {
+        service.delete(category);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Category> getAll() {
+        return service.getAll();
     }
 }
--- src/test/java/org/solidarizr/manager/controller/CategoryControllerTest.java
+++ src/test/java/org/solidarizr/manager/controller/CategoryControllerTest.java
@@ -1,5 +1,6 @@
 package org.solidarizr.manager.controller;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -7,8 +8,12 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.solidarizr.manager.model.Category;
 import org.solidarizr.manager.service.CategoryService;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 
@@ -20,6 +25,7 @@ public class CategoryControllerTest {
 
     private CategoryController controller;
 
+    @Before
     public void setUp(){
         controller = new CategoryController(service);
     }
@@ -39,5 +45,53 @@ public class CategoryControllerTest {
         Category result = controller.save(categoryToBeInserted);
 
         assertThat(result).isEqualTo(categorySaved);
+    }
+
+    @Test
+    public void should_update_category(){
+        Category categoryToBeUpdated = Category.builder()
+                .id(1)
+                .name("Category 1 Modified").build();
+
+        Category categorySaved = Category.builder()
+                .id(1)
+                .name("Category 1 Modified")
+                .build();
+
+        when(service.save(categoryToBeUpdated)).thenReturn(categorySaved);
+
+        Category result = controller.save(categoryToBeUpdated);
+
+        assertThat(result).isEqualTo(categorySaved);
+    }
+
+    @Test
+    public void should_delete_category(){
+        Category category = Category.builder()
+                .id(1)
+                .name("Category 1 Modified")
+                .build();
+
+        controller.delete(category);
+
+        verify(service).delete(category);
+    }
+
+    @Test
+    public void should_list_all_caterogies(){
+        List<Category> allCategories = Arrays.asList(
+                Category.builder()
+                        .id(1)
+                        .name("Category 1").build(),
+                Category.builder()
+                        .id(1)
+                        .name("Category 2").build());
+
+
+        when(service.getAll()).thenReturn(allCategories);
+
+        List<Category> result = controller.getAll();
+
+        assertThat(result).containsExactlyElementsOf(allCategories);
     }
 }
