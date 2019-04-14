c7d50a4d8b40dd04885e1d43ec2eb014a3c8c72e
Jeferson Decker <deckerjeferson@gmail.com>
2019-04-14 20:40:40 -0300

    Fixing things to CI

--- src/main/java/org/solidarizr/manager/model/Event.java
+++ src/main/java/org/solidarizr/manager/model/Event.java
@@ -14,10 +14,6 @@ import javax.persistence.*;
 public class Event {
 
     public static final String SEQUENCE_NAME = "event_id_seq";
-<<<<<<< HEAD
-
-=======
->>>>>>> cb1990ccc2c1194a680f270eebac232d9a228d26
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQUENCE_NAME)
     @SequenceGenerator(name = SEQUENCE_NAME, sequenceName = SEQUENCE_NAME, initialValue = 1, allocationSize = 1)
@@ -32,11 +28,7 @@ public class Event {
     private Category category;
 
     @ManyToOne
-<<<<<<< HEAD
     @JoinColumn(name = "creator")
-=======
-    @JoinColumn(name = "organization")
->>>>>>> cb1990ccc2c1194a680f270eebac232d9a228d26
     private Organization organization;
 
 }
